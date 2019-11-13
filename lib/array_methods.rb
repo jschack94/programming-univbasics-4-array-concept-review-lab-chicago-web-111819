dbf0f153fbf08078bd1d8df611754d98087bbc64
Jonathan Schack <jschack94@gmail.com>
2019-11-13 17:36:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,7 +20,10 @@ end
 def find_min_value(array)
   min = 0
     array.length.times do |count|
-     if array[count] <= min
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
           min = array[count]
         end
       end
